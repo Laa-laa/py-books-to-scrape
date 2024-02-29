0b0ad867709df58200b1a0783299ef6cdfd31334
Lelia KOZAN <lelia.kozan@ecoles-epsi.net>
2024-02-29 14:08:35 +0100

    maj dossiers

--- script_scrape_I.py
+++ script_scrape_I.py
@@ -1,9 +1,11 @@
 import script_function
+import os
 
 def main():
     url = "https://books.toscrape.com/catalogue/dune-dune-1_151/index.html"
     book_info = script_function.scrape_book_info(url)
     if book_info:
+        os.makedirs('one_book', exist_ok=True)
         script_function.save_to_csv([book_info], "one_book","one_book")
         print("Les informations ont été sauvegardées avec succès dans le fichier CSV.")
     else:
